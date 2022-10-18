b4a80aadc4c584b8a2f4657b658f65f2474c8365
enchou <dodger25685@gmail.com>
2022-10-18 11:16:55 +0800

    add array-version

--- hw1a.cpp
+++ hw1a.cpp
@@ -1,6 +1,5 @@
 # include "iostream"
 # include "iomanip"
-# include "vector"
 # include "math.h"
 using namespace std;
 
@@ -13,11 +12,17 @@ struct node{
     double x,y;
 };
 
+/*
 vector<node> guns;
-vector<node> c_guns;
+vector<node> c_guns;*/
+node guns[1000000], c_guns[1000000];
+int c_guns_size=0;
+
 int main(){
     int T;
     cin>> T ;
+
+
     for(int i=0; i<T; i++){
         insertion();
 
@@ -30,12 +35,14 @@ int main(){
 }
 
 void insertion(){
-    guns.clear();
+    //guns.clear();
     node gun;
     cin>> n;
     for(int i=0; i<n; i++){
         cin>> gun.x>> gun.y;
-        guns.push_back(gun);
+        //guns.push_back(gun);
+        guns[i]=gun;
+        
     
         //insertion
         for(int j=0; j<i; j++){
@@ -74,11 +81,14 @@ double conquer(double center, double range, int BC_l, int BC_r){
     node gun;
     min_distance=pow(pow(guns[first].x-guns[first+1].x, 2)+pow(guns[first].y-guns[first+1].y, 2),0.5);
 
-    c_guns.clear();
+    //c_guns.clear();
+    c_guns_size=0;
 
     for(int i=BC_l; i<BC_r; i++){
         if((center-range<guns[i].x)&&(guns[i].x<center+range)){
-            c_guns.push_back(guns[i]);
+            //c_guns.push_back(guns[i]);
+            c_guns[c_guns_size++]=guns[i];
+
 
             //insertion
             for(int j=0; j<i; j++){
@@ -96,17 +106,17 @@ double conquer(double center, double range, int BC_l, int BC_r){
 
 
     //Compute
-    if(c_guns.size()==0)
+    if(c_guns_size==0)//(c_guns.size()==0)
         return min_distance;
-    else if(c_guns.size()<=5){
-        for(int k=1; k<=c_guns.size()-1; k++){
+    else if(c_guns_size<=5){
+        for(int k=1; k<=c_guns_size-1; k++){
             L2_norm=pow(pow(c_guns[0].x-c_guns[k].x, 2)+pow(c_guns[0].y-c_guns[k].y, 2),0.5);
             if(L2_norm<min_distance)
                 min_distance=L2_norm;
         }
     }
     else{
-        for(int j=0; j<c_guns.size()-4; j++){
+        for(int j=0; j<c_guns_size-4; j++){
             for(int k=1; k<=4; k++){
                 L2_norm=pow(pow(c_guns[j].x-c_guns[j+k].x, 2)+pow(c_guns[j].y-c_guns[j+k].y, 2),0.5);
                 if(L2_norm<min_distance)
